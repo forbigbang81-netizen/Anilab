ae9323282d9bf0f32174fba9eac8858aee988c1a
forbigbang81-netizen <forbigbang81@gmail.com>
2026-04-02 21:26:37 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {
+      // ADD THESE TWO BLOCKS BELOW
       colors: {
         'cr-orange': '#F47521',
         'cr-black': '#000000',
@@ -12,4 +17,5 @@ module.exports = {
       },
     },
   },
+  plugins: [],
 }
