de241db3cbbc6f4454e2bb10bdd94d862a5f8ab9
forbigbang81-netizen <forbigbang81@gmail.com>
2026-04-02 21:07:58 -0500

    Delete Hero.jsx

--- components/Hero.jsx
+++
@@ -1,5 +0,0 @@
-const fetchMovies = async () => {
-  const res = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${da679b1021912724c1ed572201e2864c}`);
-  const data = await res.json();
-  return data.results;
-};
